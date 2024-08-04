e6dabae170b847542d51cf8e8e6c739d70f4fb48
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-08-04 11:42:03 +0600

    commit11

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose.connect(process.env.MONGO_URI, {
-      dbName: "MERN_HOSPITAL_MANAGEMENT",
+      dbName: "MERN_HOSPITAL_MANAGEMENT_DEPLOYED",
     })
     .then(() => {
       console.log("Connected to database!");
